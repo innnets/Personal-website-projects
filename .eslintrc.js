b730d7cf638764d6251d9d286ce53019bf4e6635
Innnets <hi@innnets.com>
2025-02-26 00:59:20 +0800

    更新 .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,25 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: [
-    // ... 其他配置
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
   ],
   plugins: [
+    '@typescript-eslint',
     'simple-import-sort'
   ],
   rules: {
     'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error'
+    'simple-import-sort/exports': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn'
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   }
 } 
